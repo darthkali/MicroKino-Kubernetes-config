a58d9b0acafb993ab75179918e0371b91d16678a
dannysteinbrecher <danny.steinbrecher@outlook.com>
2023-01-11 11:55:01 +0100

    added all services an kafka, kafka has still an error

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,20 @@ kind delete cluster --name micro-kino
 # create cluster
 kind create cluster --config kind-cluster.yml
 
+## create secrets
+kubectl apply -f dockerconfigjson.yml
+
 # create postgres
 kubectl apply -f postgres
 
+# create kafka
+kubectl apply -f kafka
+
 # create cinema-service
 kubectl apply -f cinemaservice
+kubectl apply -f bookingservice
+kubectl apply -f movieservice
+kubectl apply -f showservice
 
 # create traefik & ingressroute
 kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v2.9/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml #Traefik Resource Definitions::
